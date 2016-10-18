90fc4245b0421cf993800d254aef13f16b2bef34
Jakub Kułak <jakub.kulak@gmail.com>
2016-10-17 20:18:03 -0500

    Return formatted results

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 'use strict';
 
-let config = require('./config.js');
+const http = require("http");
+const url = require("url");
+const config = require('./config.js');
+const showGameResults = require('./lib/bot.js').showGameResults;
+const debug = require('debug')('server');
+const mustache = require('mustache');
 
-const debug = require('debug')('lol-slack-bot');
-debug('booting %s', __filename);
-debug("Environment: %s", config.ENV);
+function sendResponse(res, body) {
 
-let waterfall = require('async/waterfall');
-let leagueApi = require('./lib/riot-api-wrapper');
-let league = new leagueApi(config.RIOT_API_KEY);
+    const view = {
+        title: "Joe",
+        calc: function() {
+            return 2 + 4;
+        },
+        body
+    };
 
-let bot = require('./lib/bot.js');
+    const template = "{{title}} spends {{body}}";
+    var html = mustache.to_html(template, view);
 
-let summonerId = null;
-let summonerName = 'almostroy';
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}
 
-waterfall([
-    function (callback) {
-        league.getSummonersByName('euw', summonerName, function(err, data) {
-            summonerId = data[summonerName].id;
-            callback(null, summonerId);
-        });
-    },
-    function (summonerId, callback) {
-        league.getRecentGames('euw', summonerId, function(err, data) {
-            bot.processGames(data.games);
-            callback(null, 'done');
-        });
-    }
-], function(err, result){
-    debug(result);
-});
+http.createServer(function (req, res) {
+
+    const parsedUrl = url.parse(req.url, true); // true to get query as object
+    const queryAsObject = parsedUrl.query;
+    const summonerName = queryAsObject.summonerName;
+
+    showGameResults(summonerName, function(err, body) {
+        sendResponse(res, body);
+    });
+
+}).listen(config.WEB_SERVER.PORT);
+
+// Console will print the message
+debug('Server started!');

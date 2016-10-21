2e94cd6a7cc2785dc8df2725e8e8d2bfc46f621a
Jakub Kułak <jakub.kulak@gmail.com>
2016-10-20 23:22:37 -0500

    Use gulp to replace resource directories for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
 'use strict';
 
 const gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    babel = require('gulp-babel'),
     rename = require('gulp-rename'),
     del = require('del'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
-    size = require('gulp-size'),
     autoprefixer = require('gulp-autoprefixer'),
     imagemin = require('gulp-imagemin'),
-    cssnano = require('gulp-cssnano');
+    cssnano = require('gulp-cssnano'),
+    htmlreplace = require('gulp-html-replace'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel'),
+    size = require('gulp-size');
 
 // Use https://www.npmjs.com/package/gulp-useref for production build (updates in HTML)
 
 gulp.task('default', ['clean'], () => {
-    gulp.start('styles', 'images');
+    gulp.start('styles', 'images', 'scripts', 'update-html-dependencies');
+});
+
+gulp.task('update-html-dependencies', () => {
+    gulp.src('lib/templates/*.html')
+        .pipe(htmlreplace({
+            'css': '/s/css/main.min.css',
+            'js': '/s/js/main.min.js'
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('styles', () => {
@@ -51,25 +61,45 @@ gulp.task('clean', () => {
     return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
 });
 
-// gulp.task('scripts', () => {
-//     const s = size();
-//
-//     return gulp.src('./public/*.js')
-//         .pipe(s)
-//         .pipe(babel({
-//             presets: ['es2015']
-//         }))
-//         .pipe(jshint('.jshintrc'))
-//         .pipe(jshint.reporter('default'))
-//         .pipe(concat('main.js'))
-//         .pipe(gulp.dest('public/js'))
-//         .pipe(rename({
-//             suffix: '.min'
-//         }))
-//         .pipe(uglify())
-//         .pipe(gulp.dest('public/js'))
-//         .pipe(s)
-//         .pipe(notify({
-//             message: 'Scripts task complete'
-//         }));
-// });
+// Watch
+gulp.task('watch', () => {
+
+    // Watch .scss files
+    gulp.watch('public/css/*.css', ['styles']);
+
+    // Watch .js files
+    gulp.watch('public/js/*.js', ['scripts']);
+
+    // Watch image files
+    gulp.watch('public/images/*', ['images']);
+
+    // // Create LiveReload server
+    // livereload.listen();
+    //
+    // // Watch any files in dist/, reload on change
+    // gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
+
+gulp.task('scripts', () => {
+    //     const s = size();
+    //
+    //     return gulp.src('./public/*.js')
+    //         .pipe(s)
+    //         .pipe(babel({
+    //             presets: ['es2015']
+    //         }))
+    //         .pipe(jshint('.jshintrc'))
+    //         .pipe(jshint.reporter('default'))
+    //         .pipe(concat('main.js'))
+    //         .pipe(gulp.dest('public/js'))
+    //         .pipe(rename({
+    //             suffix: '.min'
+    //         }))
+    //         .pipe(uglify())
+    //         .pipe(gulp.dest('public/js'))
+    //         .pipe(s)
+    //         .pipe(notify({
+    //             message: 'Scripts task complete'
+    //         }));
+});

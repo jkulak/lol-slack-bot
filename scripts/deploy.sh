b6e4f66b3b52d60b61dfa0778da020fcd78ad6f8
Jakub Kułak <jakub.kulak@gmail.com>
2016-10-24 14:00:18 -0500

    Update deplotment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+mkdir build
+mv * build
+tar -czf package.tgz build
+scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null

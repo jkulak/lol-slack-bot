d4abed4383273007ade31880e03e2fbd98af130c
Jakub Kułak <jakub.kulak@gmail.com>
2016-12-11 22:46:55 -0600

    Fix docker deployment script

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -10,6 +10,7 @@ ssh -oStrictHostKeyChecking=no deploy@$DEPLOY_HOST << EOF
     docker rmi jkulak/lol-slack-bot:current || true
     docker tag jkulak/lol-slack-bot:latest jkulak/lol-slack-bot:current
     docker run -d --env RIOT_API_KEY=\$RIOT_API_KEY --env MONGO_HOST=\$LOL_SLACK_BOT_MONGO_HOST --restart always --name lol-slack-bot-web -p 20201:8081 jkulak/lol-slack-bot:current
+    echo "docker run -d --env RIOT_API_KEY=\$RIOT_API_KEY --env MONGO_HOST=\$LOL_SLACK_BOT_MONGO_HOST --restart always --name lol-slack-bot-web -p 20201:8081 jkulak/lol-slack-bot:current" >> ~/DEPLOY_COMMAND
 EOF
 
 

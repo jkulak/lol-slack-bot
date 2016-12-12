49ca710b7b17894dae25982d937f3a6a4527f88d
Jakub Kułak <jakub.kulak@gmail.com>
2016-12-11 21:24:34 -0600

    Update docker deployment script to use env vars

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -9,7 +9,7 @@ ssh -oStrictHostKeyChecking=no deploy@$DEPLOY_HOST << EOF
     docker rm lol-slack-bot-web || true
     docker rmi jkulak/lol-slack-bot:current || true
     docker tag jkulak/lol-slack-bot:latest jkulak/lol-slack-bot:current
-    docker run -d --restart always --name lol-slack-bot-web -p 20201:8081 jkulak/lol-slack-bot:current
+    docker run -d --env RIOT_API_KEY=$RIOT_API_KEY --env MONGO_HOST=$LOL_SLACK_BOT_MONGO_HOST --restart always --name lol-slack-bot-web -p 20201:8081 jkulak/lol-slack-bot:current
 EOF
 
 
